3e491b835c722c00ffa4a3232d71bfaffd0f8753
pushpa chauhan <puspa.xawn@gmail.com>
2021-12-19 15:01:57 +0545

    file added

---
+++ datatype.js
@@ -0,0 +1,25 @@
+// var apple=[ 'juicy','fresh','rotten','rippen'];
+// console.log('appple',apple[9]);
+
+// var ice=[ 'apple',122 ,true,'puspa'];
+// var	phone=999;
+	
+
+// console.log('appple',ice[1]);
+// //array 
+
+// object
+var app={
+	mobile:'samsung',
+	model:144,
+	price:666,
+	phone:{
+		telephon:98877,
+		phone:7799
+	}
+
+
+}
+console.log('result',app);
+
+
